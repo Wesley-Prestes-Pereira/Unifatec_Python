79b3838378ebfbd183b4bebecf775cb7db82c753
Wesley Prestes Pereira <73717841+Wesley-Prestes-Pereira@users.noreply.github.com>
2024-05-03 11:44:22 -0300

    Update dashboard.py

--- P1/dashboard.py
+++ P1/dashboard.py
@@ -3,11 +3,12 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-# Carregar o arquivo JSON
+# Carrega o banco de dados JSON
 url = "https://raw.githubusercontent.com/Wesley-Prestes-Pereira/Unifatec_Python/main/P1/datasheet.json"
-df = pd.read_json(url)
+data_json = json.loads(urlopen(url).read())
+df = pd.DataFrame(data_json)
 
-# Função para converter a duração em minutos
+# Adaptações necessárias
 def convert_duration_to_minutes(duration):
     if 'min' in duration:
         return sum(int(x) * 60 ** i for i, x in enumerate(reversed(duration.replace(' min', '').split(','))))
@@ -16,7 +17,10 @@ def convert_duration_to_minutes(duration):
 df['Duração_min'] = df['Duração'].apply(convert_duration_to_minutes)
 df['Tipo'] = df['Duração_min'].apply(lambda x: 'Filme' if x <= 120 else 'Série')
 
-# Funções de gráficos
+df_filmes = df[df['Tipo'] == 'Filme']
+df_series = df[df['Tipo'] == 'Série']
+
+# Definição das funções de gráficos
 def distruibuicao_por_genero(df, title):
     fig = px.bar(df, x='Genero', title=f'{title} - Distribuição por Gênero')
     return fig
@@ -33,23 +37,24 @@ def duracao_classificacao(df, title):
     fig = px.bar(df, x='Classificação', y='Duração_min', title=f'{title} - Média de Duração por Classificação')
     return fig
 
+# Definição do layout do aplicativo
 st.set_page_config(layout='wide')
 st.title("Filmes/Séries: 🎥")
 st.sidebar.title('Filtros')
-filtro_classificacao = st.sidebar.multiselect(
-    'Classificação',
-    df['Classificação'].unique(),
-)
+filtro_classificacao = st.sidebar.multiselect('Classificação', df['Classificação'].unique())
 
 if filtro_classificacao:
     df = df[df['Classificação'].isin(filtro_classificacao)]
 
 aba1, aba2, aba3 = st.columns(3)
 with aba1:
-    st.dataframe(df)
+    st.plotly_chart(distruibuicao_por_genero(df_filmes, 'Filmes'), use_container_width=True)
+    st.plotly_chart(pontuacao_por_classificacao(df_filmes, 'Filmes'), use_container_width=True)
 with aba2:
-    st.plotly_chart(distruibuicao_por_genero(df[df['Tipo'] == 'Filme'], 'Filmes'), use_container_width=True)
-    st.plotly_chart(pontuacao_por_classificacao(df[df['Tipo'] == 'Filme'], 'Filmes'), use_container_width=True)
+    st.plotly_chart(votos_por_genero(df_filmes, 'Filmes'), use_container_width=True)
+    st.plotly_chart(duracao_classificacao(df_filmes, 'Filmes'), use_container_width=True)
 with aba3:
-    st.plotly_chart(votos_por_genero(df[df['Tipo'] == 'Filme'], 'Filmes'), use_container_width=True)
-    st.plotly_chart(duracao_classificacao(df[df['Tipo'] == 'Filme'], 'Filmes'), use_container_width=True)
+    st.plotly_chart(distruibuicao_por_genero(df_series, 'Séries'), use_container_width=True)
+    st.plotly_chart(pontuacao_por_classificacao(df_series, 'Séries'), use_container_width=True)
+    st.plotly_chart(votos_por_genero(df_series, 'Séries'), use_container_width=True)
+    st.plotly_chart(duracao_classificacao(df_series, 'Séries'), use_container_width=True)
